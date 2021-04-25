95f890b71bece47025d69c74387f110045caef7e
MohammedHieba <mohamedhieba9574@gmail.com>
2021-04-25 20:14:45 +0200

    add counter

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component  {
+
+
+  render(){
+    return(
+
+
+      <div>
+      <Counter />
+    </div>
+
+    )
+ 
+  }
+ 
+}
+
+
+class Counter extends React.Component {
+
+  constructor(){
+    super()
+    this.state={
+      count:0
+    }
+  }
+
+  increase=()=>{
+    this.setState({count: this.state.count+1})
+  }
+
+  decrease=()=>{
+    this.setState({count: this.state.count-1})
+  }
+  render(){
+    return (
+
+       <div>
+          <div>{this.state.count}</div>
+          <button onClick={this.increase}>Increase</button>
+          <button onClick={this.decrease}>Decrease</button>
+       
+       
+
+
+       </div>
+
+
+    )
+  }
+  
+}
+
+
+export default App;
